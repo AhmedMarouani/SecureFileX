62590e438889872473918f97d0fdfea661ece0fe
Ahmed Marouani <ahmed.marouani@esprit.tn>
2023-03-03 21:38:03 +0100

    first commit

---
+++ Challenge1/Challenge1/src/test/java/com/example/Challenge1/Challenge1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Challenge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challenge1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
