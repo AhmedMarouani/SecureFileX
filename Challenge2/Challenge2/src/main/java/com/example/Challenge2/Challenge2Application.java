0a357021e1dc8b5b731a7345ea15f5df62244793
Ahmed Marouani <ahmed.marouani@esprit.tn>
2023-03-13 15:44:18 +0100

    404.html error page

--- Challenge2/Challenge2/src/main/java/com/example/Challenge2/Challenge2Application.java
+++ Challenge2/Challenge2/src/main/java/com/example/Challenge2/Challenge2Application.java
@@ -1,7 +1,9 @@
 package com.example.Challenge2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableScheduling
 @SpringBootApplication
 @EnableSwagger2
+
 public class Challenge2Application {
 
 	public static void main(String[] args) {
